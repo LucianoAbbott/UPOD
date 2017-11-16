76123fc7c3b0b5a2d658de47724be5ff3bfeaade
LaurenH1090 <31938233+LaurenH1090@users.noreply.github.com>
2017-11-15 21:48:48 -0500

    Add files via upload

--- Database/UPOD Database SQL.sql
+++ Database/UPOD Database SQL.sql
@@ -3,15 +3,14 @@ CREATE DATABASE UPOD;
 CREATE TABLE PAGE(
 PageId INT,
 title VARCHAR(50) NOT NULL,
-URL VARCHAR(100) NOT NULL,
+URL VARCHAR(200) NOT NULL,
 editing BOOLEAN NOT NULL,
-PRIMARY KEY (PageId)
-);
+PRIMARY KEY (PageId));
 
 CREATE TABLE EQUATION(
-equationId INT,
+equId INT,
 equationURL VARCHAR(200),
-PRIMARY KEY (equationId) );
+PRIMARY KEY (equId) );
 
 CREATE TABLE VARIABLE (
 varId INT,
@@ -22,17 +21,16 @@ description VARCHAR(400),
 PRIMARY KEY (varId) );
 
 CREATE TABLE EQUVAR(
-equVarId INT,
-equationId INT,
+equId INT,
 varId INT,
-PRIMARY KEY (equVarId),
-FOREIGN KEY (equationId) REFERENCES Equation(equationId), 
-FOREIGN KEY (varId) REFERENCES Variable(varId) );
+PRIMARY KEY (equId,varId),
+FOREIGN KEY (equId) REFERENCES EQUATION(equId), 
+FOREIGN KEY (varId) REFERENCES VARIABLE(varId) );
 
 CREATE TABLE GRAPHIC(
 graphicId INT,
 graphicURL VARCHAR (200),
-description VARCHAR (144),
+description VARCHAR (400),
 PRIMARY KEY(graphicId));
 
 CREATE TABLE SECTION(
@@ -40,10 +38,9 @@ sectionId INT,
 pageId INT,
 sectionTitle VARCHAR(100) NOT NULL,
 sectionText VARCHAR (4000),
-equationId INT,
+equId INT,
 graphicId INT,
 PRIMARY KEY(pageId,sectionId),
 FOREIGN KEY(pageId) REFERENCES PAGE(pageId),
-FOREIGN KEY(equationId) REFERENCES EQUATION(equationId),
-FOREIGN KEY(graphicId) REFERENCES GRAPHIC(graphicId)
-);
+FOREIGN KEY(equId) REFERENCES EQUATION(equId),
+FOREIGN KEY(graphicId) REFERENCES GRAPHIC(graphicId));
