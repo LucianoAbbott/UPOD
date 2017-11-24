4e93a5868ead1c93cdcc0deb2aa36e899196ff2f
LaurenH1090 <31938233+LaurenH1090@users.noreply.github.com>
2017-11-24 14:44:46 -0500

    Update Section.java

--- Backend/src/main/java/datatypes/Section.java
+++ Backend/src/main/java/datatypes/Section.java
@@ -5,4 +5,58 @@ package datatypes;
  * @author luciano abbott
  */
 public class Section {
+  
+  private int sectionId;
+	private String title;
+	private String text;
+	private Equation equ;
+	private Graphic graphic;
+	
+	public Section(int sId, String sTitle, String sText, Equation e,Graphic g){
+		
+		sectionId = sId;
+		title = sTitle;
+		text = sText;
+		equ = e;
+		graphic = g;
+		
+	} 
+	
+	public Section(){
+		
+		sectionId = 0;
+		pageId = 0;
+		title = null;
+		text = null;
+		equ = null;
+		graphic = null;
+		
+	} 
+  
+  public int getSecId(){
+		return this.sectionId;
+	}
+	
+	public String getSecTitle(){
+		return this.title;
+	}
+	
+	public String getSecText(){
+		return this.text;
+	}
+	
+	public int setSecId(int id){
+		this.sectionId = id;
+	}
+	
+	public String setSecTitle(String sTitle){
+		this.title = sTitle;
+	}
+	
+	public String setSecText(String sText){
+		this.text = sText;
+	}
+  
+  //still need setters and getters for equations and graphics
+  
 }
