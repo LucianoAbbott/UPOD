89b8d20346765e46bc2302993fb32c8ff54a5d89
Luciano Abbott <luciano@Lucianos-MacBook-Air.local>
2017-12-02 13:53:39 -0500

    Reverted bad style change

--- Backend/src/main/java/datatypes/Variable.java
+++ Backend/src/main/java/datatypes/Variable.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 /**
  * Variable object of symbol/name pair
+ * 
  * @author luciano and Nathan
  *
  */
@@ -13,58 +14,68 @@ public class Variable {
 	private String name;
 	private String description;
 	private String url;
-	private int variableId;
-	
-	public Variable (String symbol, String name,String description, String url, int id) {
+	private int id;
+
+	public Variable(String symbol, String name, String description, String url, int id) {
 		this.symbol = symbol;
 		this.name = name;
 		this.description = description;
 		this.url = url;
-		this.variableId = id;
+		this.id = id;
 	}
 
-	public Variable(){
+	public Variable() {
 		this.symbol = "";
 		this.name = "";
 		this.description = "";
 		this.url = "";
-		this.variableId = 0;
+		this.id = 0;
 	}
-	
-	public Variable (ResultSet variableResult) throws SQLException {
-		this(variableResult.getString("symbol"),variableResult.getString("name"),variableResult.getString("description"),variableResult.getString("URL"),variableResult.getInt("varId"));
+
+	public Variable(ResultSet variableResult) throws SQLException {
+		this(variableResult.getString("symbol"), variableResult.getString("name"),
+				variableResult.getString("description"), variableResult.getString("URL"),
+				variableResult.getInt("varId"));
 	}
 
 	public String getSymbol() {
-		return symbol;
+		return this.symbol;
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
-	public String getDescription(){
-		return description;	
-	}	
-	public String getURL(){
-		return url;	
+
+	public String getDescription() {
+		return this.description;
 	}
-	public int getId(){
-		return id;	
+
+	public String getURL() {
+		return this.url;
 	}
-	public void setId(int id){
-		this.variableId=id;
+
+	public int getId() {
+		return this.id;
 	}
-	public void setName(String newName){
-		this.name = newName;	
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public void setDescription(String newDescription){
+
+	public void setName(String newName) {
+		this.name = newName;
+	}
+
+	public void setDescription(String newDescription) {
 		this.description = newDescription;
 	}
-	public void setURL(String newUrl){
-		this.url = newUrl;	
+
+	public void setURL(String newUrl) {
+		this.url = newUrl;
 	}
-	public void setSymbol(String newSymbol){
-		this.symbol = newSymbol;	
+
+	public void setSymbol(String newSymbol) {
+		this.symbol = newSymbol;
 	}
-		
+
 }
