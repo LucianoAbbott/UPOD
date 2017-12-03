5f5a3a20687f3cd9b19a96cfa4fe0529a6718c66
Peter Smerchinski <smer5910@mylaurier.ca>
2017-12-03 16:24:55 -0500

    Added Function Documentation to Equation, Grpahic and Page Classes
    Tried to keep language consistent and provide more of a template so that all of our documentation can look the same.

--- Backend/src/main/java/datatypes/Graphic.java
+++ Backend/src/main/java/datatypes/Graphic.java
@@ -13,43 +13,116 @@ public class Graphic {
 	private String graphicURL; 
 	private String description; 
 	
+	/**
+	 * Constructor for class "Graphic"
+	 * Preconditions:
+	 *		(int) gId - integer number to be used to represent graphicID
+	 *		(String) gURL - String in URL format to represent graphicURL
+	 *		(String) gDescription - String to be used as the description
+	 * Postconditions:
+	 * Exceptions: None.
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public Graphic(int gId, String gURL, String gDescription){
 		
 		this.graphicId = gId;
 		this.graphicURL = gURL;
 		this.description = gDescription;
 	}
+	
+	/**
+	 * Constructor for class "Graphic"
+	 * Preconditions:
+	 * Postconditions:
+	 * Exceptions: None.
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public Graphic(){
 		this.graphicId = 0;
 		this.graphicURL = "";
 		this.description = "";
 	}
-			 
 	
+	/**
+	 * Constructor for class "Graphic"
+	 * Preconditions:
+	 * Postconditions:
+	 * Exceptions: SQLException
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public Graphic(ResultSet graphicResult) throws SQLException {
 		this(graphicResult.getInt("graphicId"), 
 				graphicResult.getString("graphicURL"),
 				graphicResult.getString("description"));
 	}
 
+	/**
+	 * Method to get the graphicId of a graphic
+	 * Preconditions:
+	 * Postconditions:
+	 * 		returns the (int) graphicId
+	 * Exceptions: None
+	 * Date last changed: 11/29/2017
+	 * @author 
+	 */
 	public int getGraphicId() {
 		return graphicId;
 	}
 
+	/**
+	 * Method to get the graphicURL of a graphic
+	 * Preconditions:
+	 * Postconditions:
+	 * 		returns the (String) graphicURL
+	 * Exceptions: None
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public String getGraphicURL() {
 		return graphicURL;
 	}
-
+	
+	/**
+	 * Method to get the description of a graphic
+	 * Preconditions:
+	 * Postconditions:
+	 * 		returns the (String) description
+	 * Exceptions: None
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Method to set the description of a graphic
+	 * Preconditions:
+	 * 		description:
+	 * Postconditions: A new (String) description is set for the graphic.
+	 * Exceptions: None
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	/**
+	 * Method to get the graphicURL of a graphic
+	 * Preconditions:
+	 * Postconditions: A new (string) graphicURL is set for the graphic.
+	 * Exceptions: None
+	 * Date last changed: 11/29/2017
+	 * @author Nathan Skof
+	 */
 	public void setGraphicURL(String gGraphicURL){
 		this.graphicURL = gGraphicURL;	
 	}
+
 	
 //	public static void  setGraphicURL(Graphic graphic,String GraphicURL){
 //		
