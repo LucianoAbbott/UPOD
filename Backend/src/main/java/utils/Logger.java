5c3705dcb14ff5a59646495bb02d3872b756c16e
Peter Smerchinski <smer5910@mylaurier.ca>
2017-12-12 14:16:13 -0500

    Updated function documentation for Utils
    Files Logger and SearchUtils are affected only in comments.
    Date last changed are wrong most likely but I will check git later to get correct dates and put them in as well as put an author name for logger based on who commited it.

--- Backend/src/main/java/utils/Logger.java
+++ Backend/src/main/java/utils/Logger.java
@@ -2,7 +2,20 @@ package utils;
 
 import datatypes.Page;
 
+/**
+ * Functions used to print system logs
+ *
+ */
 public class Logger {
+	/**
+	 * Log message to system when a page is updated
+	 * Preconditions: 
+	 *		(Page) page - the page that was updated
+	 * Postconditions: log is printed to system
+	 * Exceptions: None.
+	 * Date last changed:
+	 * @author
+	 */
 	public static void logUpdate(Page page) {
 		if (page == null) {
 			System.out.println("Error: update page with null page");
@@ -10,10 +23,29 @@ public class Logger {
 			System.out.println("Updating page #" + page.getId() + ", at URL:" + page.getUrl() + ", Title: " + page.getTitle());
 		}
 	}
+	
+	/**
+	 * Log message to system when a page is deleted
+	 * Preconditions: 
+	 *		(Page) page - the page that was deleted
+	 * Postconditions: log is printed to system
+	 * Exceptions: None.
+	 * Date last changed:
+	 * @author
+	 */
 	public static void logDelete(int id) {
 		System.out.println("Deleting page #" + id);
 	}
-
+	
+	/**
+	 * Log message to system when a page is requested
+	 * Preconditions: 
+	 *		(Page) page - the page that was requested
+	 * Postconditions: log is printed to system
+	 * Exceptions: None.
+	 * Date last changed:
+	 * @author
+	 */
 	public static void logGet(Page page) {
 		if (page == null) {
 			System.out.println("Error: get page returned null");
@@ -22,6 +54,15 @@ public class Logger {
 		}
 	}
 
+	/**
+	 * Log message to system when a page is search for with a query string.
+	 * Preconditions: 
+	 *		(String) query - search query used
+	 * Postconditions: log is printed to system
+	 * Exceptions: None.
+	 * Date last changed:
+	 * @author
+	 */
 	public static void logSearch(String query) {
 		if (query == null) {
 			System.out.println("Error: search performed with bad query");
