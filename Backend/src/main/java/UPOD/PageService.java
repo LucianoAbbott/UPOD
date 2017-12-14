6ea60d8b6ecd6d8ee8fa56bd75c6e9c7b148d524
TravisLeyenaarMisson <31947202+TravisLeyenaarMisson@users.noreply.github.com>
2017-12-14 11:36:13 -0500

    Package declaration cleanups
    now just one UPOD

--- Backend/src/main/java/UPOD/PageService.java
+++ Backend/src/main/java/UPOD/PageService.java
@@ -1,4 +1,4 @@
-package com.teamUPOD.UPOD.UPOD;
+package UPOD;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
--- Backend/src/main/java/UPOD/UpodDao.java
+++ Backend/src/main/java/UPOD/UpodDao.java
@@ -1,4 +1,4 @@
-package com.teamUPOD.UPOD.UPOD;
+package UPOD;
 
 import java.io.FileInputStream;
 import java.io.IOException;
