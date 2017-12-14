2c813bfb336d5f4e71fa9d8f169575896252dc93
nathankohler <kohler.nathan@gmail.com>
2017-12-14 14:01:51 -0500

    merge
    v

--- Front End Proposals/HTML/flex/UPOD.js
+++ Front End Proposals/HTML/flex/UPOD.js
@@ -492,15 +492,8 @@ function helloWorld() {
 // SORRY FOR THIS GUYS NEED TO BE ABLE TO PARSE SOME SHIT
 // MY BAD
 // Was giving me errors so I'm hiding it for now sorry :( Nick~
-/*
 function parseJSON() {
     // CALL JSON FROM DB
-<<<<<<< HEAD
-    let data = '{"title": "Waves","sections": {"section1": {"secTitle": "Description","content": {"text": "The general body of text accompanying a section","image": "href to image stored in server","diagram": "href to diagram for ifram insert","equation": "latex for mathjax"}},"section2": {"secTitle": "General"content": "text": "The general body of text accompanying a section"image": "href to image stored in server  "diagram": "href to diagram for ifram insert"equation": "latex for mathjax"section3": "secTitle": "Properties"content": "text": "The general body of text accompanying a section","image": "href to image stored in server" diagram": "href to diagram for ifram insert" equation": "latex for mathjax}}}}'
-    window.sessionStorage.data=JSON.stringify(data);
-    if(window.sessionStorage){alert(JSON.parse(sessionStorage.data))};
-    
-=======
     let data = '{"title": "Waves","sections": {"section1": {"secTitle": "Description","content": {"text": "The general body of text accompanying a section","image": "href to image stored in server","diagram": "href to diagram for ifram insert","equation": "latex for mathjax"}},"section2": {"secTitle": "General"content": "text": "The general body of text accompanying a section"image": "href to image stored in server  "diagram": "href to diagram for ifram insert"equation": "latex for mathjax"section3": "secTitle": "Properties"content": "text": "The general body of text accompanying a section","image": "href to image stored in server" diagram": "href to diagram for ifram insert" equation": "latex for mathjax}}}}';
     window.sessionStorage.data=JSON.stringify(data);
     if(window.sessionStorage){alert(JSON.parse(sessionStorage.data))};
@@ -508,4 +501,3 @@ function parseJSON() {
 
     //todo submit to data base yo
 }
-*/
