1f7204817050e4593b835f8a77504e69ea8e53a8
nathankohler <kohler.nathan@gmail.com>
2017-12-14 13:21:43 -0500

    lol
    lol

--- Front End Proposals/HTML/flex/UPOD.js
+++ Front End Proposals/HTML/flex/UPOD.js
@@ -495,9 +495,9 @@ function helloWorld() {
 function parseJSON() {
     // CALL JSON FROM DB
     let data = '{"title": "Waves","sections": {"section1": {"secTitle": "Description","content": {"text": "The general body of text accompanying a section","image": "href to image stored in server","diagram": "href to diagram for ifram insert","equation": "latex for mathjax"}},"section2": {"secTitle": "General"content": "text": "The general body of text accompanying a section"image": "href to image stored in server  "diagram": "href to diagram for ifram insert"equation": "latex for mathjax"section3": "secTitle": "Properties"content": "text": "The general body of text accompanying a section","image": "href to image stored in server" diagram": "href to diagram for ifram insert" equation": "latex for mathjax}}}}'
-
     window.sessionStorage.data=JSON.stringify(data);
     if(window.sessionStorage){alert(JSON.parse(sessionStorage.data))};
     
+
     //todo submit to data base yo
 }
