da31780428ede397e62304438a9a82048f46aa58
nathankohler <kohler.nathan@gmail.com>
2017-12-21 01:08:11 -0500

    uh
    uh

--- Front End Proposals/HTML/flex/UPOD.js
+++ Front End Proposals/HTML/flex/UPOD.js
@@ -1,5 +1,4 @@
 "use strict";
-
 var i = 0;
 
 function addSection() {
@@ -502,13 +501,9 @@ function helloWorld() {
 // SORRY FOR THIS GUYS NEED TO BE ABLE TO PARSE SOME SHIT
 // MY BAD
 // Was giving me errors so I'm hiding it for now sorry :( Nick~
-/*
-function parseJSON() {
-    // CALL JSON FROM DB
-    let data = '{"title": "Waves","sections": {"section1": {"secTitle": "Description","content": {"text": "The general body of text accompanying a section","image": "href to image stored in server","diagram": "href to diagram for ifram insert","equation": "latex for mathjax"}},"section2": {"secTitle": "General"content": "text": "The general body of text accompanying a section"image": "href to image stored in server  "diagram": "href to diagram for ifram insert"equation": "latex for mathjax"section3": "secTitle": "Properties"content": "text": "The general body of text accompanying a section","image": "href to image stored in server" diagram": "href to diagram for ifram insert" equation": "latex for mathjax}}}}';
-    window.sessionStorage.data=JSON.stringify(data);
-    if(window.sessionStorage){alert(JSON.parse(sessionStorage.data))};
 
-    //todo submit to data base yo
+function parseJSON(pageJSON) {
+    // CALL JSON FROM DB
+    var jsonCon= document.createElement("div");
+    var
 }
-*/
